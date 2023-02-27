a3f91d7d6bce1b0976f94e41182ee43daf9baf3c
moonnuitstudio <alexanderlguard98@gmail.com>
2023-02-26 16:11:33 -0800

    test

--- src/components/gui/cards/dashboard/RecentContactCards.jsx
+++ src/components/gui/cards/dashboard/RecentContactCards.jsx
@@ -6,40 +6,54 @@ import {
     List,
     ListItem,
     ListItemButton,
-    ListItemText
+    ListItemText,
+    CardActions,
+    IconButton
 } from "@mui/material"
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 
 import useContacts from "../../../../hooks/useContacts"
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
+import { useEffect } from "react";
+
+import phone_book from '../../../../assets/img/phone_book.png'
+
+const btnLinkStyle = {
+    cursor: 'pointer',
+    color: '#4A4E69',
+}
 
 const RecentContactCards = () => {
 
     const { contacts } = useContacts()
     const navigate = useNavigate()
 
-  return (
-    <Box sx={{ width: '100%', background: '#9A8C98', padding: '10px', borderRadius: '2px', boxShadow: '2px 2px 30px -15px rgba(0,0,0,0.75)' }}>
-        <CardContent>
-            <Typography sx={{color: 'white', textTransform: 'uppercase', fontSize: '15px'}} fontWeight={700} className="fnt-roboto" variant="h5" component="div">Recent Contacts</Typography>
-            <Divider sx={{background: 'white', marginBottom: '10px'}} />
-            <Box sx={{width: '100%'}}>
-                <List>
-                    {contacts.slice(0, 5).map(contact => {
-                        const {id, firstname, lastname} = contact
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', height: '300px', background: '#f2f2f2', padding: '10px', borderRadius: '2px', border: '1px solid rgba(0,0,0,.2)', zIndex: 10 }}>
+            <CardContent sx={{paddingBottom: '0px', flexGrow: 1}}>
+                <img src={phone_book} width="50px" style={{ float: 'right' }} />
+                <Typography sx={{color: 'black', textTransform: 'uppercase', fontSize: '15px'}} fontWeight={700} className="fnt-roboto" variant="h5" component="div">Recent Contacts</Typography>
+                <Divider width="20%" sx={{background: '#4A4E69', marginBottom: '10px', height: '3px'}} />
+                <Box sx={{width: '100%'}}>
+                    
+                        {contacts.slice(0, 5).map(contact => {
+                            const {id, firstname, lastname} = contact
 
-                        return (
-                        <ListItem key={id} disablePadding>
-                            <ListItemButton onClick={_ => { navigate(`/contacts/${id}`);  console.log(`/contacts/${id}`)}}>
-                                <ListItemText className="fnt-montserrat" sx={{color: 'white'}} primary={`${firstname} ${lastname}`} />
-                            </ListItemButton>
-                        </ListItem>)
-                    })}
-                </List>
-            </Box>
-        </CardContent>
-    </Box>
-  )
+                            return (
+                                <p><a className="fnt-montserrat color-2-hover underline-hover" style={btnLinkStyle} onMouseDown={_ => navigate(`/contacts/${id}`)}>{`${firstname} ${lastname}`}</a></p>
+                            )
+                        })}
+                    
+                </Box>
+            </CardContent>
+            <CardActions sx={{borderTop: '1px solid rgba(0,0,0,0.2)', paddingBottom: '0px'}} disableSpacing>
+                <IconButton onMouseDown={_ => navigate(`/contacts`)} aria-label="add to favorites">
+                    <ContactPhoneIcon />
+                </IconButton>
+            </CardActions>
+        </Box>
+    )
 }
 
 export default RecentContactCards
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,44 @@
-import GridLayout, { WidthProvider, Responsive } from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
 import { useOutletContext } from 'react-router-dom';
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { 
+  Box
+} from "@mui/material"
 
 import RecentContactCards from "../components/gui/cards/dashboard/RecentContactCards";
 
+import { useState } from "react";
+
 const ResponsiveReactGridLayout  = WidthProvider(Responsive)
 
-const Dashboard = () => {
+const Dashboard = ({}) => {
    // layout is an array of objects, see the demo for more complete usage
-   const [isMobileOrTable, containerWidth] = useOutletContext()
+    const [isMobileOrTable, containerWidth] = useOutletContext()
+    const { isAuthenticated } = useAuth0()
 
-   const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 1 },
-    { i: "b", x: 1, y: 0, w: 1, h: 1 },
-    { i: "c", x: 4, y: 0, w: 1, h: 1 }
-  ];
+    const layout = [
+        { i: "1", x: 0, y: 0, w: 1, h: 1 },
+    ];  
 
-  return (
-    <GridLayout
-        className="layout"
-        layout={layout}
-        cols={3}
-        rowHeight={30}
-        width={containerWidth}
-      >
-        <div key="a"><RecentContactCards /></div>
-      </GridLayout>
-  )
+    if (!isAuthenticated) return null
+
+    return (
+      <Box sx={{width: "100%", paddingTop: '30px'}}>
+        <ResponsiveReactGridLayout
+            measureBeforeMount={true}
+            rowHeight={300}
+            className="layout"
+            layout={layout}
+            breakpoints={{ lg: 1100, md: 900, sm: 600 }}
+            cols={{ lg: 3, md: 2, sm: 1 }}
+            width={containerWidth}
+          >
+            <div key="a1"><RecentContactCards /></div>
+        </ResponsiveReactGridLayout>
+      </Box>
+    )
 }
 
 export default Dashboard
